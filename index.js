c69413db94506e90a51da610f36f8b971f50a05b
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2018-01-21 14:30:07 -0500

    add base files

---
+++ index.js
@@ -0,0 +1,101 @@
+class Table {
+  constructor(name, schema) {
+    this.name = name;
+    this.cache = {};
+  }
+
+}
+
+class Database {
+    //Database General
+    constructor(name) {
+        this.name = name;
+        this.cache = {};
+        this.tables = {};
+    }
+    persistDb() {
+      var vault = this.decodeDb(window.localStorage.localVault);
+      vault[this.name] = this.encodeDb(this.cache);
+      window.localStorage.localVault= this.encodeDb(vault);
+    }
+    refreshCache() {
+      this.cache = this.decodeDb(this.decodeDb(window.localStorage.localVault)[this.name]);
+    }
+    encodeDb(db) {
+      return btoa(btoa(JSON.stringify(db)));
+    }
+    decodeDb(db) {
+      return JSON.parse(atob(atob(db)));
+    }
+
+    //Tables
+    createTable(name, schema) {
+
+    }
+    dropTable(name) {
+
+    }
+    dropTables() {
+
+    }
+    listTables() {
+
+    }
+    fetchTable(name) {
+
+    }
+
+}
+
+class DatabaseApi {
+    static init() {
+      if (!window.localStorage.hasOwnProperty('localVault')) {
+        window.localStorage.localVault = this.encode({});
+      }
+      console.log(window.localStorage);
+    }
+    static encode(vault) {
+      return btoa(btoa(JSON.stringify(vault)));
+    }
+    static decode(vault) {
+      return JSON.parse(atob(atob(vault)));
+    }
+    static create(name) {
+      if (!this.doesExist(name)) {
+        var db = new Database(name);
+        db.persistDb();
+        return db;
+      } else {
+        throw Error('Database ' + name + ' already exists!  Try fetch("' + name + '") method instead.');
+      }
+    }
+    static fetch(name) {
+      if (!this.doesExist(name)) {
+        throw Error('Database ' + name + ' does not exist!  Try create("' + name  + '") method instead');
+      } else {
+        var db = new Database(name);
+        db.refreshCache();
+        return db;
+      }
+    }
+    static list() {
+      return Object.keys(this.decode(window.localStorage.localVault));
+    }
+    static destroy(name) {
+      if (this.doesExist(name)) {
+        var store = this.decode(window.localStorage.localVault);
+        delete store[name];
+        console.log(store);
+        window.localStorage.localVault = this.encode(store);
+      } else {
+        throw Error('Database ' + name + ' could not be destroyed!  Database ' + name + ' does not exist.')
+      }
+    }
+    static doesExist(name) {
+        return this.decode(window.localStorage.localVault).hasOwnProperty(name);
+    }
+    static destroyAll() {
+      delete window.localStorage.localVault;
+      console.log(window.localStorage);
+    }
+}
---
+++ src/Database.js
@@ -0,0 +1,34 @@
+export default class Database {
+    //Database General
+    constructor(name) {
+        this.name = name;
+        this.cache = {};
+    }
+    persistDb() {
+
+    }
+    refreshCache() {
+
+    }
+    encodeDb() {
+
+    }
+    decodeDb() {
+
+    }
+
+    //Tables
+    createTable(name) {
+
+    }
+    dropTable(name) {
+
+    }
+    dropTables() {
+
+    }
+    fetchTable(name) {
+
+    }
+
+}
